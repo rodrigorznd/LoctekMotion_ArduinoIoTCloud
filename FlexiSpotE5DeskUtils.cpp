d6c12e2b3e9dc17a41d435e5b64bfc348789dc00
Rodrigo Rezende <rezendr@amazon.com>
2021-05-07 00:02:07 +0100

    Add initial code

---
+++ FlexiSpotE5DeskUtils.cpp
@@ -0,0 +1,121 @@
+#include "Arduino.h"
+#include "FlexiSpotE5DeskUtils.h"
+
+DeskDisplay::DeskDisplay() {}
+
+void DeskDisplay::setDebug(Serial_* debug) {
+  this->_debug = debug;
+}
+
+String DeskDisplay::byteToString(byte dig) {
+  String dp = "";
+  String v = "";
+  if(dig > _dp_value) {
+    dp = ".";
+    dig -= _dp_value;
+  }
+  
+  for(int i = 0; i < _len; i++) {
+    if(_k[i] == byte(dig)) {
+      v = _v[i];
+      break;
+    }
+  }
+  
+  if (_debug && v == "") {
+    _debug->println("Fail to decode: ");
+    _debug->print("BIN: ");
+    _debug->println(dig, BIN);
+    _debug->print("HEX: ");
+    _debug->println(dig, HEX);
+    _debug->println("-----------------");
+  }
+  return v + dp;
+}
+
+String DeskDisplay::bytesToString(byte *digs) { // Define this limit where?
+  String v = "";
+  for(int i = 3; i < 3 + this->numDigits; i++) {
+    v += this->byteToString(digs[i]);
+  }
+  
+  if (_debug) _debug->print("Result: ");
+  if (_debug) _debug->println(v);
+  
+  return v;
+}
+
+Desk::Desk(Uart* uart, byte oin20Pin, byte ledPin) { 
+  this->_uart = uart;
+  this->_oin20Pin = oin20Pin;
+  this->_ledPin = ledPin;
+  init();
+}
+    
+void Desk::setDebug(Serial_* debug) {
+  this->_debug = debug;
+}
+
+void Desk::init() {
+  pinMode(_oin20Pin, OUTPUT);
+  pinMode(_ledPin, OUTPUT);
+  digitalWrite(_oin20Pin, HIGH);
+  digitalWrite(_ledPin, HIGH);
+}
+
+void Desk::standBy() {
+  _standByStatus = true;
+  digitalWrite(_oin20Pin, LOW);
+  digitalWrite(_ledPin, LOW);
+}
+
+void Desk::sendCommand(DeskCommand c) {
+  if (_standByStatus) {
+    _standByStatus = false;
+    digitalWrite(_oin20Pin, HIGH);
+    digitalWrite(_ledPin, HIGH);
+    delay(500);
+  }
+
+  if (_debug) _debug->print("Sending command: ");
+  if (_debug) _debug->println(deskCommandsNames[c]);
+  if (_debug) _debug->print("TX: ");
+  for (int i = 0; i < commandLength; i++) {
+    _uart->write(deskCommands[c][i]);
+    if (_debug) _debug->print(deskCommands[c][i], HEX);
+    if (_debug) _debug->print(" ");
+  }
+  if (_debug) _debug->println("");
+}
+
+void Desk::sendIdleCommand() {
+  sendCommand(IDLE);
+}
+
+void Desk::sendUpCommand() {
+  sendCommand(UP);
+}
+
+void Desk::sendDownCommand() {
+  sendCommand(DOWN);
+}
+
+void Desk::sendOneCommand() {
+  sendCommand(ONE);
+}
+
+void Desk::sendTwoCommand() {
+  sendCommand(TWO);
+}
+
+void Desk::sendThreeCommand() {
+  sendCommand(THREE);
+}
+
+void Desk::sendMemoryCommand() {
+  sendCommand(MEMORY);
+}
+
+void Desk::sendAlarmCommand() {
+  sendCommand(ALARM);
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+## Flexispot / Loctekmotion Standing Desk - Arduino IoT Cloud 
+
+Yet another project to transform a Flexispot / Loctekmotion standing desk in a smart standing desk.
+
+## Components
+- Desk Model: [FLEXISPOT E5W Standing Desk](https://amzn.to/3tnmQmR)
+- Control Box Model: [CB38M2A-1](https://www.loctekmotion.cn/shop/control-system/cb38m2a/)
+- Control Panel Model: [HS01B-1](https://www.loctekmotion.cn/shop/control-system/hs05a-1/) 
+- Arduino Board: [Arduino MKR WiFi 1010](https://amzn.to/2QZBCmK)
+
+## Similar projects / research
+
+While working on this project, I found out that I am not the only one with this idea. There are a few repositories on GitHub with great research which helped me kickstart this project.️
+
+- [grssmnn / ha-flexispot-standing-desk](https://github.com/grssmnn/ha-flexispot-standing-desk) - Home Assistant integration via MQTT (micropython)
+- [Dude88 / loctek_IOT_box](https://github.com/Dude88/loctek_IOT_box) - Arduino code to control via Alexa and MQTT 
+- [nv1t / standing-desk-interceptor](https://github.com/nv1t/standing-desk-interceptor) - Research on intercepting commands from Flexispot desks
+- [VinzSpring / LoctekReverseengineering](https://github.com/VinzSpring/LoctekReverseengineering#assumptions) - Research and Python samples
+- [iMicknl / LoctekMotion_IoT](https://github.com/iMicknl/LoctekMotion_IoT) - LoctekMotion/FlexiSpot desk into a smart desk
