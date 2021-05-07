40e47ab69bd9fd4bcdb8d6a798b36b13009fa8cd
Rodrigo Rezende <rezendr@amazon.com>
2021-05-08 00:48:37 +0100

    Formating...

--- FlexiSpotE5DeskUtils.h
+++ FlexiSpotE5DeskUtils.h
@@ -4,74 +4,95 @@
 #include "Arduino.h"
 
 class DeskDisplay {
-  private:
+private:
     inline static constexpr int _len = 20;
-    inline static constexpr byte  _k[_len] = {0x00, 0x09, 0x40, 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x71, 0x31, 0x37};
-    inline static constexpr char* _v[_len] = { " ",  "=",  "-",  "0",  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",  "A",  "F",  "R",  "n"};
-    inline static constexpr byte _dp_value = 0x80;
-    Serial_* _debug = NULL;
-    
-  public:
+    inline static constexpr byte
+    _k[_len] = { 0x00, 0x09, 0x40, 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x71, 0x31, 0x37 };
+    inline static constexpr char *_v[_len] = {" ", "=", "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "F",
+                                              "R", "n"};
+    inline static constexpr byte
+    _dp_value = 0x80;
+    Serial_ *_debug = NULL;
+
+public:
     static const int numDigits = 3;
     static const byte displayMessageLength = 0x07;
-    
+
     DeskDisplay();
-    void setDebug(Serial_* debug);
+
+    void setDebug(Serial_ *debug);
+
     String byteToString(byte dig);
+
     String bytesToString(byte *digs);
-    
+
 };
 
 class Desk {
-  
-  private:
-    Serial_* _debug = NULL;
-    Uart* _uart;
+
+private:
+    Serial_ *_debug = NULL;
+    Uart *_uart;
     byte _oin20Pin;
     byte _ledPin;
     bool _standByStatus = true;
 
-  public:
-    enum DeskCommand {IDLE, UP, DOWN, ONE, TWO, THREE, MEMORY, ALARM};
+public:
+    enum DeskCommand {
+        IDLE, UP, DOWN, ONE, TWO, THREE, MEMORY, ALARM
+    };
 
     inline static constexpr char deskCommandsNames[8][8] = {
-      "IDLE",
-      "UP",
-      "DOWN",
-      "ONE",
-      "TWO",
-      "THREE",
-      "MEMORY",
-      "ALARM",
+            "IDLE",
+            "UP",
+            "DOWN",
+            "ONE",
+            "TWO",
+            "THREE",
+            "MEMORY",
+            "ALARM",
     };
 
     static const int commandLength = 8;
     static const byte messageStart = 0x9B;
     static const byte messageEnd = 0x9D;
-    
-    inline static constexpr byte deskCommands[8][commandLength] = {
-      {messageStart, 0x06, 0x02, 0x00, 0x00, 0x6C, 0xA1, messageEnd}, // IDLE
-      {messageStart, 0x06, 0x02, 0x01, 0x00, 0xFC, 0xA0, messageEnd}, // UP
-      {messageStart, 0x06, 0x02, 0x02, 0x00, 0x0C, 0xA0, messageEnd}, // DOWN
-      {messageStart, 0x06, 0x02, 0x04, 0x00, 0xAC, 0xA3, messageEnd}, // 1
-      {messageStart, 0x06, 0x02, 0x08, 0x00, 0xAC, 0xA6, messageEnd}, // 2
-      {messageStart, 0x06, 0x02, 0x10, 0x00, 0xAC, 0xAC, messageEnd}, // 3
-      {messageStart, 0x06, 0x02, 0x20, 0x00, 0xAC, 0xB8, messageEnd}, // Memory
-      {messageStart, 0x06, 0x02, 0x40, 0x00, 0xAC, 0x90, messageEnd}, // Alarm
+
+    inline static constexpr byte
+    deskCommands[8][commandLength] = {
+        { messageStart, 0x06, 0x02, 0x00, 0x00, 0x6C, 0xA1, messageEnd }, // IDLE
+        { messageStart, 0x06, 0x02, 0x01, 0x00, 0xFC, 0xA0, messageEnd }, // UP
+        { messageStart, 0x06, 0x02, 0x02, 0x00, 0x0C, 0xA0, messageEnd }, // DOWN
+        { messageStart, 0x06, 0x02, 0x04, 0x00, 0xAC, 0xA3, messageEnd }, // 1
+        { messageStart, 0x06, 0x02, 0x08, 0x00, 0xAC, 0xA6, messageEnd }, // 2
+        { messageStart, 0x06, 0x02, 0x10, 0x00, 0xAC, 0xAC, messageEnd }, // 3
+        { messageStart, 0x06, 0x02, 0x20, 0x00, 0xAC, 0xB8, messageEnd }, // Memory
+        { messageStart, 0x06, 0x02, 0x40, 0x00, 0xAC, 0x90, messageEnd }, // Alarm
     };
-    
-    Desk(Uart* uart, byte oin20Pin, byte ledPin);
-    void setDebug(Serial_* debug);
+
+    Desk(Uart *uart, byte oin20Pin, byte ledPin);
+
+    void setDebug(Serial_ *debug);
+
     void init();
+
     void standBy();
+
     void sendCommand(DeskCommand c);
+
     void sendIdleCommand();
+
     void sendUpCommand();
+
     void sendDownCommand();
+
     void sendOneCommand();
+
     void sendTwoCommand();
+
     void sendThreeCommand();
+
     void sendMemoryCommand();
+
     void sendAlarmCommand();
 };
 
--- LoctekMotion_ArduinoIoTCloud.ino
+++ LoctekMotion_ArduinoIoTCloud.ino
@@ -52,173 +52,173 @@ DeskDisplay deskDisplay = DeskDisplay();
 
 
 void setup() {
-  // Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  Serial1.begin(9600);
+    // Initialize serial and wait for port to open:
+    Serial.begin(9600);
+    Serial1.begin(9600);
 
-  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
-  delay(6000);
-  
-  // Defined in thingProperties.h
-  initProperties();
+    // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+    delay(6000);
 
-  // Connect to Arduino IoT Cloud
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+    // Defined in thingProperties.h
+    initProperties();
 
-  /*
-     The following function allows you to obtain more information
-     related to the state of network and IoT Cloud connection and errors
-     the higher number the more granular information you’ll get.
-     The default is 0 (only errors).
-     Maximum is 4
-  */
-  setDebugMessageLevel(4);
-  ArduinoCloud.printDebugInfo();
-  
-  pinMode(UP_PIN, INPUT_PULLUP);
-  pinMode(DOWN_PIN, INPUT_PULLUP);
-  pinMode(ONE_PIN, INPUT_PULLUP);
-  pinMode(TWO_PIN, INPUT_PULLUP);
-  pinMode(THREE_PIN, INPUT_PULLUP);
-  pinMode(MEMORY_PIN, INPUT_PULLUP);
-  pinMode(ALARM_PIN, INPUT_PULLUP);
-  
-  desk.setDebug(&Serial);
-  deskDisplay.setDebug(&Serial);
+    // Connect to Arduino IoT Cloud
+    ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+
+    /*
+       The following function allows you to obtain more information
+       related to the state of network and IoT Cloud connection and errors
+       the higher number the more granular information you’ll get.
+       The default is 0 (only errors).
+       Maximum is 4
+    */
+    setDebugMessageLevel(4);
+    ArduinoCloud.printDebugInfo();
+
+    pinMode(UP_PIN, INPUT_PULLUP);
+    pinMode(DOWN_PIN, INPUT_PULLUP);
+    pinMode(ONE_PIN, INPUT_PULLUP);
+    pinMode(TWO_PIN, INPUT_PULLUP);
+    pinMode(THREE_PIN, INPUT_PULLUP);
+    pinMode(MEMORY_PIN, INPUT_PULLUP);
+    pinMode(ALARM_PIN, INPUT_PULLUP);
+
+    desk.setDebug(&Serial);
+    deskDisplay.setDebug(&Serial);
 }
 
 void loop() {
-  ArduinoCloud.update();
-  
-  delay(100);
-  while (Serial1.available() > 0) {
-    readControlBoxMessage();
-  }
-  
-  readButtonCommand();
-  
-  if (cmd != Desk::IDLE || btn_cmd != Desk::IDLE) {
-    cycles = MAX_CYCLES;
-    if (btn_cmd) {
-      desk.sendCommand(btn_cmd);
-    } else {
-      desk.sendCommand(cmd);
+    ArduinoCloud.update();
+
+    delay(100);
+    while (Serial1.available() > 0) {
+        readControlBoxMessage();
     }
-  } else {
-    if (cycles > 0) {
-      desk.sendIdleCommand();
-      cycles--;
+
+    readButtonCommand();
+
+    if (cmd != Desk::IDLE || btn_cmd != Desk::IDLE) {
+        cycles = MAX_CYCLES;
+        if (btn_cmd) {
+            desk.sendCommand(btn_cmd);
+        } else {
+            desk.sendCommand(cmd);
+        }
     } else {
-      desk.standBy();
+        if (cycles > 0) {
+            desk.sendIdleCommand();
+            cycles--;
+        } else {
+            desk.standBy();
+        }
     }
-  }
 }
 
 
 void readControlBoxMessage() {
-  // read the incoming byte:
-  incomingByte = Serial1.read();
+    // read the incoming byte:
+    incomingByte = Serial1.read();
 
-  buffer[bufferPosition] = incomingByte;
-  if (incomingByte == Desk::messageEnd) {
-    if (buffer[1] == DeskDisplay::displayMessageLength) {
-      display = deskDisplay.bytesToString(buffer);
+    buffer[bufferPosition] = incomingByte;
+    if (incomingByte == Desk::messageEnd) {
+        if (buffer[1] == DeskDisplay::displayMessageLength) {
+            display = deskDisplay.bytesToString(buffer);
+        }
+        bufferPosition = 0;
+    } else {
+        bufferPosition++;
     }
-    bufferPosition = 0;
-  } else {
-    bufferPosition++;
-  }
 }
 
 void readButtonCommand() {
-  if (!digitalRead(UP_PIN)) {
-    btn_cmd = Desk::UP;
-  } else if (!digitalRead(DOWN_PIN)) {
-    btn_cmd = Desk::DOWN;
-  } else if (!digitalRead(ONE_PIN)) {
-    btn_cmd = Desk::ONE;
-  } else if (!digitalRead(TWO_PIN)) {
-    btn_cmd = Desk::TWO;
-  } else if (!digitalRead(THREE_PIN)) {
-    btn_cmd = Desk::THREE;
-  } else if (!digitalRead(MEMORY_PIN)) {
-    btn_cmd = Desk::MEMORY;
-  } else if (!digitalRead(ALARM_PIN)) {
-    btn_cmd = Desk::ALARM;
-  } else {
-    btn_cmd = Desk::IDLE;
-  }
+    if (!digitalRead(UP_PIN)) {
+        btn_cmd = Desk::UP;
+    } else if (!digitalRead(DOWN_PIN)) {
+        btn_cmd = Desk::DOWN;
+    } else if (!digitalRead(ONE_PIN)) {
+        btn_cmd = Desk::ONE;
+    } else if (!digitalRead(TWO_PIN)) {
+        btn_cmd = Desk::TWO;
+    } else if (!digitalRead(THREE_PIN)) {
+        btn_cmd = Desk::THREE;
+    } else if (!digitalRead(MEMORY_PIN)) {
+        btn_cmd = Desk::MEMORY;
+    } else if (!digitalRead(ALARM_PIN)) {
+        btn_cmd = Desk::ALARM;
+    } else {
+        btn_cmd = Desk::IDLE;
+    }
 }
 
 
 void onDownChange() {
-  Serial.print("Value of down changed to: ");
-  Serial.println(down);
-  if (down) {
-    cmd = Desk::DOWN;
-  } else {
-    cmd = Desk::IDLE;
-  }
+    Serial.print("Value of down changed to: ");
+    Serial.println(down);
+    if (down) {
+        cmd = Desk::DOWN;
+    } else {
+        cmd = Desk::IDLE;
+    }
 }
 
 void onOneChange() {
-  Serial.print("Value of one changed to: ");
-  Serial.println(one);
-  if (one) {
-    cmd = Desk::ONE;
-  } else {
-    cmd = Desk::IDLE;
-  }
+    Serial.print("Value of one changed to: ");
+    Serial.println(one);
+    if (one) {
+        cmd = Desk::ONE;
+    } else {
+        cmd = Desk::IDLE;
+    }
 }
 
 void onThreeChange() {
-  Serial.print("Value of three changed to: ");
-  Serial.println(three);
-  if (three) {
-    cmd = Desk::THREE;
-  } else {
-    cmd = Desk::IDLE;
-  }
+    Serial.print("Value of three changed to: ");
+    Serial.println(three);
+    if (three) {
+        cmd = Desk::THREE;
+    } else {
+        cmd = Desk::IDLE;
+    }
 }
 
 void onTwoChange() {
-  Serial.print("Value of down changed two: ");
-  Serial.println(two);
-  if (two) {
-    cmd = Desk::TWO;
-  } else {
-    cmd = Desk::IDLE;
-  }
+    Serial.print("Value of down changed two: ");
+    Serial.println(two);
+    if (two) {
+        cmd = Desk::TWO;
+    } else {
+        cmd = Desk::IDLE;
+    }
 }
 
 void onUpChange() {
-  Serial.print("Value of down changed up: ");
-  Serial.println(up);
-  if (up) {
-    cmd = Desk::UP;
-  } else {
-    cmd = Desk::IDLE;
-  }
+    Serial.print("Value of down changed up: ");
+    Serial.println(up);
+    if (up) {
+        cmd = Desk::UP;
+    } else {
+        cmd = Desk::IDLE;
+    }
 }
 
 
 void onMemoryChange() {
-  Serial.print("Value of memory changed up: ");
-  Serial.println(memory);
-  if (memory) {
-    cmd = Desk::MEMORY;
-  } else {
-    cmd = Desk::IDLE;
-  }
+    Serial.print("Value of memory changed up: ");
+    Serial.println(memory);
+    if (memory) {
+        cmd = Desk::MEMORY;
+    } else {
+        cmd = Desk::IDLE;
+    }
 }
 
 
 void onAlarmChange() {
-  Serial.print("Value of alarm changed up: ");
-  Serial.println(alarm);
-  if (alarm) {
-    cmd = Desk::ALARM;
-  } else {
-    cmd = Desk::IDLE;
-  }
+    Serial.print("Value of alarm changed up: ");
+    Serial.println(alarm);
+    if (alarm) {
+        cmd = Desk::ALARM;
+    } else {
+        cmd = Desk::IDLE;
+    }
 }
